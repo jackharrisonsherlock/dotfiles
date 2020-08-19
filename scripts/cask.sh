4b81b99344cfbba105803094c162d3e9a28b732e
Jack Harrison-Sherlock <jack@harrisonsherlock.com>
2020-08-20 10:53:51 +1200

    update brews

--- scripts/cask.sh
+++ scripts/cask.sh
@@ -11,19 +11,21 @@ brew cask install firefox
 # Development
 brew cask install visual-studio-code
 brew cask install atom
-brew cask install iterm2-nightly
 brew cask install hyper
-brew cask install dash
 brew cask install postman
 brew cask install sequel-pro
 brew cask install docker
 brew cask install aws-vault
 brew cask install ngrok
 
+# Design
+brew cask install figma
+
 # Communication
 brew cask install slack
 brew cask install whatsapp
 brew cask install teamviewer
+brew cask install loom
 
 # Cloud
 brew cask install dropbox
@@ -34,16 +36,20 @@ brew cask install alfred
 brew cask install kap
 brew cask install 1password
 brew cask install openoffice
+brew cask install notion
 
 # Media
-brew cask install plex-media-server
 brew cask install catch
 brew cask install spotify
+brew cask install vlc
 brew cask install transmission
 
 # Mac OS Enhancements
 brew cask install bartender
 brew cask install spectacle
+brew cask install rocket
+brew cask install hazel
+brew cask install bettertouchtool
 brew tap buo/cask-upgrade
 
 # Other
