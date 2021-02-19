4f356587972bab2db48da3b080085113812a2395
Jack Harrison-Sherlock <jack@harrisonsherlock.com>
2021-02-19 14:52:46 +1300

    update terminal setup

--- dots/.hyper.js
+++ dots/.hyper.js
@@ -7,115 +7,122 @@ module.exports = {
         // Choose either "stable" for receiving highly polished,
         // or "canary" for less polished but more frequent updates
         updateChannel: 'stable',
-
+  
+        hyperTransparent: {
+          backgroundColor: '#170D32',
+          opacity: 0.9,
+          vibrancy: 'ultra-dark' // ['', 'dark', 'medium-light', 'ultra-dark']
+        },
+  
         // default font size in pixels for all tabs
         fontSize: 15,
-
+  
         // font family with optional fallbacks
         fontFamily: '"SF Mono", "Hack Nerd Font"',
-
+  
         // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
         cursorColor: '#72767c',
-
+  
         // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
         cursorShape: 'BLOCK',
-
+  
         // set to true for blinking cursor
         cursorBlink: false,
-
+  
         // color of the text
-        foregroundColor: '#bcc4d1',
-
+        foregroundColor: '#FFFFFF',
+  
         // terminal background color
-        backgroundColor: '#282d35',
-
+        backgroundColor: '#170D32',
+  
         // border color (window, tabs)
-        borderColor: '#333',
-
+        borderColor: '#170D32',
+  
         // custom css to embed in the main window
         css: '',
-
+  
         // custom css to embed in the terminal window
         termCSS: '',
-
+  
         // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
         // default: `false` on Linux, `true` on Windows (ignored on macOS)
         showHamburgerMenu: '',
-
+  
         // set to `false` if you want to hide the minimize, maximize and close buttons
         // additionally, set to `'left'` if you want them on the left, like in Ubuntu
         // default: `true` on windows and Linux (ignored on macOS)
         showWindowControls: '',
-
+  
         hyperTabs: {
             trafficButtons: true,
         },
-
+  
         // custom padding (css format, i.e.: `top right bottom left`)
-        padding: '20px 20px 0px 20px',
-
+        padding: '10px 30px',
+  
         // the full list. if you're going to provide the full color palette,
         // including the 6 x 6 color cubes and the grayscale map, just provide
         // an array here instead of a color map object
         colors: {
-            black: '#3e4452',
-            red: '#E36C6C',
-            green: '#a7cb8b',
-            yellow: '#fdd65e',
-            blue: '#72767c',
-            magenta: '#d290e4',
-            cyan: '#90e4db',
-            white: '#d0d0d0',
-            lightBlack: '#3e4452',
-            lightRed: '#dbab79',
-            lightGreen: '#a7cb8b',
-            lightYellow: '#fdd65e',
-            lightBlue: '#72767c',
-            lightMagenta: '#d290e4',
-            lightCyan: '#90e4db',
+            black: '#A0A0A0',
+            red: '#FF6363',
+            green: '#54E3B0',
+            yellow: '#FFF065',
+            blue: '#6599FF',
+            magenta: '#F8518D',
+            cyan: '#49E8F2',
+            white: '#FFFFFF',
+            lightBlack: '#A0A0A0',
+            lightRed: '#FF6363',
+            lightGreen: '#54E3B0',
+            lightYellow: '#FFF065',
+            lightBlue: '#6599FF',
+            lightMagenta: '#F8518D',
+            lightCyan: '#49E8F2',
             lightWhite: '#ffffff'
         },
-
+  
         // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
         // if left empty, your system's login shell will be used by default
         shell: '',
-
+  
         // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
         // by default ['--login'] will be used
         shellArgs: ['--login'],
-
+  
         // for environment variables
         env: {},
-
+  
         // set to false for no bell
         bell: 'SOUND',
-
+  
         // if true, selected text will automatically be copied to the clipboard
         copyOnSelect: false
-
+  
         // if true, on right click selected text will be copied or pasted if no
         // selection is present (true by default on Windows)
         // quickEdit: true
-
+  
         // URL to custom bell
         // bellSoundURL: 'http://example.com/bell.mp3',
-
+  
         // for advanced config flags please refer to https://hyper.is/#cfg
     },
-
+  
     // a list of plugins to fetch and install from npm
     // format: [@org/]project[#version]
     // examples:
     //   `hyperpower`
-    plugins: ['hyper-tabs-enhanced'],
-
+    plugins: ['hyper-tabs-enhanced', "hyper-hide-title", "hyperclean", 'hyper-transparent'],
+  
+  
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
     localPlugins: [],
-
+  
     keymaps: {
         // Example
         // 'window:devtools': 'cmd+alt+o',
     }
-};
+  };
--- dots/.zshrc
+++ dots/.zshrc
@@ -1,5 +1,5 @@
 # oh-my-zsh
-export ZSH=/Users/jackharrison-sherlock/.oh-my-zsh
+export ZSH=/Users/jack/.oh-my-zsh
 
 # Theme
 ZSH_THEME="common"
@@ -7,20 +7,8 @@ ZSH_THEME="common"
 # Aliases
 source $HOME/.aliases 
 
-# Colorls
-source $(dirname $(gem which colorls))/tab_complete.sh
-
-# Disable auto-setting terminal title.
-   DISABLE_AUTO_TITLE="true"
-  function precmd () {
-  window_title="\033]0;${PWD##*/}\007"
-  echo -ne "$window_title"
-
-  function title(){
-  TITLE="\[\e]2;$*\a\]"
-  echo -e ${TITLE}
-}
-}
+#Plugins
+plugins=(git vscode)
 
 # Editor
 export EDITOR=nano
@@ -29,10 +17,6 @@ export EDITOR=nano
 export GOPATH=$HOME/code/go
 export PATH=$PATH:$GOPATH/bin
 
-# Ruby
-eval "$(rbenv init -)"
-export PATH="/usr/local/opt/ruby/bin:$PATH"
-
 # SSH
 export PATH="/usr/local/sbin:$PATH"
 
@@ -40,10 +24,17 @@ export PATH="/usr/local/sbin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use # This loads nvm
 
-#Plugins
-plugins=(git, vscode)
-plugins=(zsh-autosuggestions zsh-syntax-highlighting)
+# Homebrew
+export PATH=/opt/homebrew/bin:$PATH
+export PATH="/opt/homebrew/sbin:$PATH"
 
-source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+# rbenv
+export RBENV_ROOT=/opt/homebrew/opt/rbenv
+export PATH=$RBENV_ROOT/bin:$PATH
+eval "$(rbenv init -)"
 
+
+source $(dirname $(gem which colorls))/tab_complete.sh
+source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source $ZSH/oh-my-zsh.sh
